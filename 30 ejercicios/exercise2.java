cbd34cc834793a2bf0217d59a4b35ec1fb038538
Nicolasruiz918 <nruizsastoque@gmail.com>
2025-05-30 20:45:51 -0500

    Add files via upload

---
+++ 30 ejercicios/exercise2.java
@@ -0,0 +1,9 @@
+public class exercise2 {
+    public static void promedio() {
+
+        todos.calcularInt=(todos.numero1 + todos.numero2 + todos.numero3)/3;
+
+        System.out.println("La nota final es: "+todos.calcularInt);
+
+    }
+}
---
+++ 30 ejercicios/exercise30.java
@@ -0,0 +1,10 @@
+public class exercise30 {
+
+    public static todos Todos = new todos();
+
+    public static void Promedio_two(){
+        
+        todos.calcularInt=(todos.numero1+todos.numero2);
+        System.out.println("Promedio: "+ todos.calcularInt/2);
+    }
+}
